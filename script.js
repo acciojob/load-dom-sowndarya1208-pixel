b9ba9a29852a6aa73e64c53680a061d35d30fa4d
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-12-19 21:20:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//your JS code here. If required.
-document.addEventListener("load",() =>{
-	console.log("DOM load success");
-})
+// This event fires as soon as the HTML is fully parsed
+document.addEventListener("DOMContentLoaded", () => {
+    // Create a text node or set body text
+    document.body.textContent = "DOM load success";
+});
